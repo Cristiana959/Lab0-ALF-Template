980c61069dcd0ed726aace39ea4f2cbf0ec0b8d3
Cristiana959 <cristiana699@gmail.com>
2023-02-26 17:55:49 +0200

    Added template

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    // TODO 4: Ecrivez la solution d'exercice 4 ici
+
+    public static void main(String[] args) {
+        // TODO 1: Ecrivez la solution d'exercice 1 ici
+
+        // TODO 2: Ecrivez la solution d'exercice 2 ici
+
+        // TODO 3: Ecrivez la solution d'exercice 3 ici
+    }
+}
+
+/* TODO 5: Pour exercice 5 vous devez creer et implementer tous le classes necessaires. Pour tester vous pouvez utiliser
+   cette classe ou une classe separee */
